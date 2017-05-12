7f543be53d569ab4cc6cf753f243c118e7e47aff
Marc Abramowitz <abramowi@adobe.com>
2017-05-12 15:58:52 -0700

    Escape % in message and values
    Fixes: https://github.com/adobe-community/issues-ethos/issues/4284

--- formatters/kv.go
+++ formatters/kv.go
@@ -19,9 +19,18 @@ func KVEntryString(entry *logrus.Entry) string {
 		keys = append(keys, k)
 	}
 
+	// Escape % if it appears in value, so Sprintf doesn't try to expand it
+	entry.Message = strings.Replace(entry.Message, "%", "%%", -1)
+
 	strentry := fmt.Sprintf("MESSAGE='%s'", entry.Message)
 	for _, k := range keys {
 		v := entry.Data[k]
+		// Escape % if it appears in value, so Sprintf doesn't try to expand it
+		s, ok := v.(string)
+		if ok {
+			s = strings.Replace(s, "%", "%%", -1)
+			v = s
+		}
 		strentry = fmt.Sprintf("%s %s='%+v'", strentry, strings.ToUpper(k), v)
 	}
 
